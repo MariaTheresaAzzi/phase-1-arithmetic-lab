7755bd52a484845115c6fbf9503551852173c775
Iam maria theresa <iam.mariatheresaelkazzi@gmail.com>
2022-08-05 11:42:04 +0300

    done

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let num3 = 22;
+let num4 = 6;
+
+var multiply = num1 * num2;
+
+var random = Math.floor(Math.random()) + 1;
+console.log(random);
+
+var mod = num3 % num4;
+
+
+var max = Math.max(1, 2, 3, 4, 20);
